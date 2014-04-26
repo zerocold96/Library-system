a4448edf0fdace035d629aa74534604f2eba36ff
wturnerrun <wturnerrun@gmail.com>
2014-04-26 10:05:21 -0400

    Update and rename MyAccount.java to UpdateMyAccount.java

--- UpdateMyAccount.java
+++ UpdateMyAccount.java
@@ -9,17 +9,13 @@ public class MyAccount extends JPanel
 private String userID;
 private String userPass;
 private int userCategoryID;
-/*
-//User Objects of Entities
-Student student=new Student(userType,userID,userPass);
-Admin admin = new Admin(userType,userID,userPass);
-Faculty faculty= new Faculty(userType,userID,userPass);
-Librarian librarian = new Librarian(userType,userID,userPass);
+
+
 //Media Objects of Entities
-Books book = new Book();
-CDs cd= new CDs();
-Movies movie = new Movies();
-*/
+Book book = new Book();
+CD cd= new CD();
+Movie movie = new Movie();
+
 private JButton loginButton = new JButton("LOGIN");
 private JPanel loginPanel = new JPanel();
 private JTextField loginIDText = new JTextField(15);
@@ -27,15 +23,17 @@ private JPasswordField passwordText = new JPasswordField(15);
 private JLabel userLabel = new JLabel("LOGIN ID: ");
 private JLabel passLabel = new JLabel("PASSWORD: ");
 
-//private String[] users = {"STUDENT","FACULTY","LIBRARIAN","ADMINISTRATOR"};
-//private JComboBox category = new JComboBox(users);
+private String[] users = {"STUDENT","FACULTY","LIBRARIAN","ADMINISTRATOR"};
+private JComboBox category = new JComboBox(users);
 
-private ButtonGroup category= new ButtonGroup();
+
+
+/*private ButtonGroup category= new ButtonGroup();
 private JRadioButton studentButton= new JRadioButton("STUDENT");
 private JRadioButton facultyButton= new JRadioButton("FACULTY");
 private JRadioButton librarianButton= new JRadioButton("LIBRARIAN");
 private JRadioButton adminButton= new JRadioButton("ADMINISTRATOR");
-
+*/
 MyAccount(){
 
 	loginPanel.add(userLabel);
@@ -45,10 +43,10 @@ MyAccount(){
 	loginPanel.add(loginButton);
 	loginButton.addActionListener(new LoginListener());
 
-	studentButton.addActionListener(new RadioButtonListener());
-	facultyButton.addActionListener(new RadioButtonListener());
-	librarianButton.addActionListener(new RadioButtonListener());
-	adminButton.addActionListener(new RadioButtonListener());
+	/*studentButton.addActionListener(new LoginListener());
+	facultyButton.addActionListener(new LoginListener());
+	librarianButton.addActionListener(new LoginListener());
+	adminButton.addActionListener(new LoginListener());
 
 	category.add(studentButton);
 	category.add(facultyButton);
@@ -59,7 +57,9 @@ MyAccount(){
 	loginPanel.add(studentButton);
 	loginPanel.add(facultyButton);
 	loginPanel.add(librarianButton);
-	loginPanel.add(adminButton);
+	loginPanel.add(adminButton);*/
+
+	loginPanel.add(category);
     this.add(loginPanel);
 
 
@@ -71,13 +71,38 @@ private class LoginListener implements ActionListener
 		{
 		            userID = loginIDText.getText();
 		            userPass = passwordText.getText();
+		            //User Objects of Entities
+					Student student=new Student(userID,userPass);
+					Admin admin = new Admin(userID,userPass);
+					Faculty faculty= new Faculty(userID,userPass);
+					Librarian librarian = new Librarian(userID,userPass);
 
-		            if(userID.equals("test") && userPass.equals("12345"))
+
+
+
+		          if(student.isLoggedin())
 		            {
-		                Welcome studentProfile =new Welcome();
-		                studentProfile.setVisible(true);
-		            }
-		            else
+						StudentPage studentProfile=new StudentPage();
+						studentProfile.setVisible(true);
+					}
+				  else if(faculty.isLoggedin())
+					{
+						FacultyPage facultyProfile=new FacultyPage();
+						facultyProfile.setVisible(true);
+					}
+				  else if(librarian.isLoggedin())
+					{
+						LibrarianPage librarianProfile=new LibrarianPage();
+						librarianProfile.setVisible(true);
+					}
+				  else if(admin.isLoggedin())
+					{
+						AdminPage adminProfile=new AdminPage();
+						adminProfile.setVisible(true);
+					}
+
+
+				  else
 		            {
 						JOptionPane.showMessageDialog(null,"Invalid LoginID or Password");
 		                loginIDText.setText("");
@@ -88,21 +113,11 @@ private class LoginListener implements ActionListener
         }
 	}
 }
-private class RadioButtonListener implements ActionListener
-{
-	public void actionPerformed(ActionEvent buttonSelected)
-	{
-		if (buttonSelected.getSource()== studentButton)
-		{
-			 Welcome studentProfile =new Welcome();
-			 studentProfile.setVisible(true);
 
-		}
-	}
-}
 
 
 }
+
 
 
 
