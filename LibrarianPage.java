419d1c83a1e0afada4be9febf9deed573055e48d
wturnerrun <wturnerrun@gmail.com>
2014-04-26 10:25:13 -0400

    Create LibrarianPage.java

---
+++ LibrarianPage.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class LibrarianPage extends JFrame {
+
+public static void main(String[] args) {
+LibrarianPage frameTabel = new LibrarianPage();
+}
+
+JLabel welcome = new JLabel("Welcome to a The Library System");
+JPanel panel = new JPanel();
+
+LibrarianPage(){
+super("LibrarianPage");
+setSize(900,600);
+setLocation(26,70);
+panel.setLayout (null);
+
+welcome.setBounds(70,50,350,60);
+
+panel.add(welcome);
+
+getContentPane().add(panel);
+setVisible(true);
+}
+
+}
